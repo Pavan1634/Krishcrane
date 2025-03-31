7759dee774a0a3ae664f4dd12580e10c873fbdaf
pavan <ypavan096@gmail.com>
2025-03-31 14:43:52 +0530

    Your commit message

--- src/components/Contact/ContactPage.jsx
+++ src/components/Contact/ContactPage.jsx
@@ -1,17 +1,83 @@
 import React from "react";
-import "./ContactPage.css";
-import Navbar from "../Navbar/Navbar";
-import Footer from "../Footer/Footer";
-import Placeholder from "../../assets/PageUC.png";
+import "./Contact.css";
+import TitleDark from "../Title/TitleDark";
 
-const ContactPage = () => {
+const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "a0d49f71-7f37-4250-b252-bbf0180d98ad");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
-    <div>
-      <Navbar />
-      <img className="error-page" src={Placeholder} alt="" />
-      <Footer />
+    <div className="contact-bg">
+      <TitleDark subTitle="Contact Us" title="Get in Touch" />
+      <div className="contact">
+        <div className="contact-col">
+          <h3>Send us a message</h3>
+          <p>
+            Feel free to reach out through our contact form or find our contact
+            information below. Your inquiries, project requirements, and
+            feedback are valuable to us as we strive to deliver top-quality
+            crane solutions and exceptional service.
+          </p>
+          <ul>
+            <li>Email: sales@krishcranes.com</li>
+            <li>Phone: +91 95748 79646</li>
+            <li>
+              Plot No. A1/419/1, G.I.D.C. Estate, Vitthal Udhyognagar, <br />
+              Anand, Gujarat, India.
+            </li>
+          </ul>
+        </div>
+        <div className="contact-col">
+          <form onSubmit={onSubmit}>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Enter your name" />
+            <label>Contact</label>
+            <input
+              type="tel"
+              name="phone"
+              placeholder="Enter your phone number"
+            />
+            <label>Message</label>
+            <textarea
+              name="message"
+              rows="6"
+              placeholder="Write your message here"
+              required
+            ></textarea>
+            <button type="submit" className="btn">
+              Send
+            </button>
+          </form>
+          <span>{result}</span>
+        </div>
+      </div>
     </div>
   );
 };
+
+
+
 
 export default ContactPage;
